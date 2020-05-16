33d33aaec4b0eee240e34163859d03f2603d2814
teeyong <teeyong@users.noreply.github.com>
2020-05-16 13:36:10 +0800

    login

---
+++ app/include/User.php
@@ -0,0 +1,71 @@
+<?php
+
+class Student {
+
+    private $nric;
+    private $password;
+    private $name;
+    private $race;
+    private $DOB;
+    private $Sex;
+    private $CountryOfBirth;
+    private $Address;
+    private $email;
+    private $contactNo;
+    
+    public function __construct($nric, $password, $name, $race, $DOB, $Sex, $CountryOfBirth, $Address, $email, $contactNo) {
+        $this->nric = $nric;
+        $this->password = $password;
+        $this->name = $name;
+        $this->race = $race;
+        $this->DOB = $DOB;
+        $this->Sex= $Sex;
+        $this->CountryOfBirth = $CountryOfBirth;
+        $this->Address = $Address;
+        $this->email = $email;
+        $this->contactNo = $contactNo;
+    }
+    
+    public function getNric() {
+        return $this->nric;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getRace() {
+        return $this->race;
+    }
+
+    public function getDOB() {
+        return $this->DOB;
+    }
+
+    public function getSex() {
+        return $this->Sex;
+    }
+
+    public function getCountryOfBirth() {
+        return $this->CountryOfBirth;
+    }
+
+    public function getAddress() {
+        return $this->Address;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function getContactNo(){
+        return $this->contactNo;
+    }
+
+}
+
+?>
---
+++ app/include/UserDAO.php
@@ -0,0 +1,119 @@
+<?php
+
+require_once 'common.php';
+
+class UserDAO {
+
+    public function authenticate($nric, $password) { // Authenticate Login
+        
+        // Connect to Database
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+
+        // Write & Prepare SQL Query (take care of Param Binding if necessary)
+        $sql = "SELECT * FROM USER WHERE nric=:nric";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':nric',$nric,PDO::PARAM_STR);
+        
+        // Execute SQL Query
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $status=$stmt->execute();
+
+        // Retrieve Query Results (if any)
+        $return_message = 'Invalid NRIC! Please try again or create new account.';
+        if ($row=$stmt->fetch()){
+            if (password_verify($password,$row['password'])){
+                $return_message= 'SUCCESS';
+            }
+            else{
+                $return_message='Password is incorrect!';
+            }
+        }
+        
+        // Clear Resources $stmt, $pdo
+        $stmt = null;
+        $conn = null;
+
+        // Return (if any)
+        return $return_message;
+    }
+
+    public function retrieveUser($nric) { // Retrieve User Information
+        
+        // Connect to Database
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+
+        // Write & Prepare SQL Query (take care of Param Binding if necessary)
+        $sql = "SELECT * FROM USER WHERE nric=:nric";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':userid',$userid,PDO::PARAM_STR);
+        
+        // Execute SQL Query
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+
+        // Retrieve Query Results (if any)
+        // $user=[];
+        $user = false;
+        if ($row=$stmt->fetch()){
+            $user = new User($row['nric'], $row['password'], $row['name'], $row['race'], $row['DOB'],$row['Sex'],$row['CountryOfBirth'],$row['Address'],$row['email'],$row['contactNo']);
+        }
+        
+        // Clear Resources $stmt, $pdo
+        $stmt = null;
+        $conn = null;
+
+        // Return (if any)
+        return $user;
+    }
+
+    public function add($User) { // Adding in new user information
+        // Connect to Database
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+
+        $nric=$User->getNric();
+        $password=$User->getPassword();
+        $name=$User->getName();
+        $race=$User->getRace();
+        $DOB=$User->getDOB();
+        $sex=$User->getSex();
+        $CountryOfBirth=$User->getCountryOfBirth();
+        $address=$User->getAddress();
+        $email=$User->getEmail();
+        $contactNo=$User->getContactNo();
+
+        // Prepare SQL
+        $sql = "INSERT INTO USER (nric, password, name, race, DOB, sex, CountryOfBirth, address, email, contactNo) VALUES
+        (:userid, :password, :name, :race, :DOB, :sex, :CountryOfBirth, :address, :email, :contactNo)"; 
+        $stmt=$conn->prepare($sql);
+        $stmt->bindParam(':nric',$userid,PDO::PARAM_STR);
+        $stmt->bindParam(':password',$password,PDO::PARAM_STR);
+        $stmt->bindParam(':name',$name,PDO::PARAM_STR);
+        $stmt->bindParam(':race',$name,PDO::PARAM_STR);
+        $stmt->bindParam(':DOB',$DOB,PDO::PARAM_STR);
+        $stmt->bindParam(':sex',$sex,PDO::PARAM_STR);
+        $stmt->bindParam(':CountryOfBirth',$CountryOfBirth,PDO::PARAM_STR);
+        $stmt->bindParam(':address',$address,PDO::PARAM_STR);
+        $stmt->bindParam(':email',$email,PDO::PARAM_STR);
+        $stmt->bindParam(':contactNo',$contactNo,PDO::PARAM_STR);
+
+        // Run Query
+        $status = False;
+        if ($stmt->execute()){
+            $status=True;
+        }
+
+        // Close Query/Connection
+        $stmt = null;
+        $conn = null;
+         
+        return $status; // Boolean True or False
+    }
+    
+   
+}
+
+
+?>
